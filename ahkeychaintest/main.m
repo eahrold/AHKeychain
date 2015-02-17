e8a961dc3c09a9dd91c8101531718569ecb1f8a7
Eldon Ahrold <eldon.ahrold@gmail.com>
2015-02-16 22:22:36 -0600

    Update system keychain tests

--- ahkeychaintest/main.m
+++ ahkeychaintest/main.m
@@ -9,31 +9,52 @@
 #import <Foundation/Foundation.h>
 #import "AHKeychain.h"
 
+AHKeychainItem *newItem(){
+    AHKeychainItem *item = [[AHKeychainItem alloc] init];
+    item.service = @"com.eeaapps.test.systemkc";
+    item.label = @"AHKeychain Test System Keychain Item";
+    item.account = @"daemonapp";
+    return item;
+}
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         NSError *error;
-        AHKeychainItem *item = [[AHKeychainItem alloc]init];
-        item.service = @"com.eeaapps.test.systemkc";
-        item.label = @"AHKeychain Test System Keychain Item";
-        item.account = @"daemonapp";
+
+        // Save
+        AHKeychainItem *item = newItem();
         item.password = @"mySy5kcpaSs";
-        
         if(![[AHKeychain systemKeychain] saveItem:item error:&error]){
-            NSLog(@"Save Error: %@",error.localizedDescription);
+            NSLog(@"Save1 Error: %@",error.localizedDescription);
         }
-        
-        item.password = nil;
-        
-        if(![[AHKeychain systemKeychain] getItem:item error:&error])
-            NSLog(@"Get Error [%ld]: %@",error.code,error.localizedDescription);
+
+        // Get
+        AHKeychainItem *getItem = newItem();
+        if(![[AHKeychain systemKeychain] getItem:getItem error:&error])
+            NSLog(@"Get1 Error [%ld]: %@",error.code,error.localizedDescription);
         else
-            NSLog(@"Password from Item: %@",item.password);
-        
-//        if(![[AHKeychain systemKeychain] deleteItem:item error:&error])
-//            NSLog(@"Remove Error [%ld]: %@",error.code,error.localizedDescription);
-        
+            NSLog(@"Password from Item: %@",getItem.password);
+
+        // Re-save
+        AHKeychainItem *item2 = newItem();
+        item2.password = @"myNewSecret Password";
+        if(![[AHKeychain systemKeychain] saveItem:item2 error:&error]){
+            NSLog(@"Save2 Error: %@",error.localizedDescription);
+        }
+
+        // Get again
+        AHKeychainItem *getItemAgain = newItem();
+        if(![[AHKeychain systemKeychain] getItem:getItemAgain error:&error])
+            NSLog(@"Get2 Error [%ld]: %@",error.code,error.localizedDescription);
+        else
+            NSLog(@"Password from Item: %@",getItemAgain.password);
+
+        // Delete
+        if(![[AHKeychain systemKeychain] deleteItem:newItem() error:&error])
+            NSLog(@"Remove Error [%ld]: %@",error.code,error.localizedDescription);
+
     }
     return 0;
 }
